8311a2ca5cb5ea2a58ef0cf13087c48e8841f796
ComradeBlin <gombai.gergely@gmail.com>
2026-02-03 12:38:21 +0100

    Add UI for VAT settings

--- BTCPayServer.Plugins.VAT/obj/Release/net8.0/BTCPayServer.Plugins.VAT.AssemblyInfo.cs
+++ BTCPayServer.Plugins.VAT/obj/Release/net8.0/BTCPayServer.Plugins.VAT.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BTCPayServer.Plugins.VAT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff86077b82ee7524dd5e52290eddf0194d7d3e1f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9428916557d9784fc4262472f29fef5ab85f372")]
 [assembly: System.Reflection.AssemblyProductAttribute("BTCPayServer.Plugins.VAT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BTCPayServer.Plugins.VAT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
